8d525f61ef85909805918609a0d5a01bd60a369d
LOA <118371674+Gbevon@users.noreply.github.com>
2024-02-02 16:21:48 +0000

    Add files via upload

---
+++ commandes/social_chat.js
@@ -0,0 +1,28 @@
+const { zokou } = require('../framework/zokou');
+
+zokou(
+    {
+        nomCom: 'social_chat.js',
+        categorie: 'world of hiden blue'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg } = commandeOptions;
+
+        if (!arg[0]) {
+            repondre(` ➖ 🌐📱SOCIAL CHAT 💬
+➖➖➖➖➖➖➖➖➖
+📱CHAT💬:
+
+
+
+
+
+
+
+
+➖➖➖➖➖➖➖➖➖
+
+🌐WORLD OF HIDEN BLUE 🌐 `);
+        }
+    }
+);
